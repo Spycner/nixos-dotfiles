92b223da291955eef95d74f0165dc64f412107fc
Spycner <pascal98kraus@gmail.com>
2024-04-30 09:41:04 +0200

    refactor docker

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -1,8 +1,9 @@
-{ pkgs }:{
+{
   imports = [
     ./fonts.nix
     ./home-manager.nix
     ./xdg.nix
+    ./docker.nix
   ];
 
   programs = {
@@ -13,12 +14,4 @@
 
     seahorse.enable = true;
   };
-
-  virtualisation.docker = {
-    enable = true;
-    enableOnBoot = true;
-  };
-
-  home.packages = [ pkgs.docker-compose ];
-
 }
