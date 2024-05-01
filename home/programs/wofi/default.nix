7261251a160f994856bda23ffd6d4c09588e802c
pkraus <pascal98kraus@gmail.com>
2024-05-01 09:04:32 +0200

    rework wofi as program

--- home/programs/wofi/default.nix
+++ home/programs/wofi/default.nix
@@ -1,11 +1,41 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, input, config, ... }:
+{
+  programs.wofi = {
+    enable = true;
+    style = "* {
+        all: unset;
+        font-family: "JetBrainsMono";
+        font-size: 16px;
+      }
 
-with lib;
-let cfg = config.modules.wofi;
+      #window {
+        background-color: #292a37;
+        border-radius: 12px;
+      }
 
-in {
-    options.modules.wofi = { enable = mkEnableOption "wofi"; };
-    config = mkIf cfg.enable {
-        home.file.".config/wofi.css".source = ./wofi.css;
-    };
+      #outer-box {
+        background-color: #292a37
+        border: 4px solid #44465c;
+        border-radius: 12px;
+      }
+
+      #input{
+        margin: 1rem;
+        padding: 0.5rem;
+        border-radius: 10px;
+        background-color: #303241;
+      }
+
+      #entry {
+        margin: 0.25rem 0.75rem 0.25rem 0.75rem;
+        padding: 0.25rem 0.75rem 0.25rem 0.75rem;
+        color: #9699b7;
+        border-radius: 8px;
+      }
+
+      #entry:selected {
+        background-color: #303241;
+        color: #d9e0ee;
+      }";
+  };
 }
